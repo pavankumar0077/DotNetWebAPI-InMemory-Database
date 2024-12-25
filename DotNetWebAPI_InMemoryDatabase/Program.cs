79f81fe1cab1b24f071356614b4d3c3aab386a89
Pavan Kumar Dasari <40380941+pavankumar0077@users.noreply.github.com>
2024-12-25 08:59:04 +0000

    Updated dockerfile and cs program

--- DotNetWebAPI_InMemoryDatabase/Program.cs
+++ DotNetWebAPI_InMemoryDatabase/Program.cs
@@ -5,29 +5,49 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IBooks,BookServices>();
+builder.Services.AddScoped<IBooks, BookServices>();
 builder.Services.AddDbContext<BookContext>(options =>
-options.UseInMemoryDatabase("Book"));
+    options.UseInMemoryDatabase("Book"));
 
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Modified to work in both Development and Docker environments
+if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Docker")
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(c =>
+    {
+        c.RouteTemplate = "swagger/{documentName}/swagger.json";
+    });
+    
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "DotNetWebAPI_InMemoryDatabase API V1");
+        c.RoutePrefix = "swagger";
+    });
 }
 
+// Enable CORS
+app.UseCors();
+
+// Add these middleware components in this specific order
+app.UseRouting();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
